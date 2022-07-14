5ad680aaaee040c97b8d061d8c83b3706cab439a
ViktorSivashev <viktor.sivashev@mail.ru>
2022-07-14 20:32:59 +0300

    Task2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,8 +6,8 @@
 
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Наше случайное число = {number}");
-int digit1 = number / 10;
-int digit2 = number % 10;
+int digit1 = number / 10; //48 / 10 = 4
+int digit2 = number % 10; //48 % 10 = 8
 if (digit1 > digit2)
 {
     Console.WriteLine($"Цифра {digit1} наибольшая");
---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Наше случайное число = {number}");
+int digit1 = number / 100;
+int digit2 = number / 10 % 10;
+int digit3 = number % 10;
+int result = digit1 * 10 + digit3;
+Console.WriteLine(result);
+//Console.WriteLine($"Третье число = {digit3}");
+
